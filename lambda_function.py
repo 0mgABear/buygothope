773ade5e93dc7e1579de4c522a5b4d8404467c66
0mgABear <yeoyi94@gmail.com>
2026-01-13 11:47:04 +0800

    Mode 1

--- lambda_function.py
+++ lambda_function.py
@@ -15,34 +15,40 @@ def send_telegram(text):
     r.raise_for_status()
 
 def lambda_handler(event, context):
+    mode = event.get("mode")
     token = os.environ.get("BROWSERLESS_TOKEN")
     if not token:
         return {"statusCode": 500, "body": "Missing Browserless token"}
 
     ws = f"wss://chrome.browserless.io?token={token}"
 
-    with sync_playwright() as p:
-        browser = p.chromium.connect_over_cdp(ws)
-        context_pw = browser.new_context()
-        page = context_pw.new_page()
+    if mode == "next_draw":
+        with sync_playwright() as p:
+            browser = p.chromium.connect_over_cdp(ws)
+            context_pw = browser.new_context()
+            page = context_pw.new_page()
 
-        url = "https://www.singaporepools.com.sg/en/product/pages/toto_results.aspx"
-        page.goto(url, wait_until="domcontentloaded", timeout=20000)
-        next_jackpot = page.locator("xpath=//div[normalize-space()='Next Jackpot']/following-sibling::span[1]").inner_text().strip()
-        jackpot = "$" + "".join(c for c in next_jackpot if c.isdigit() or c == ",")
-        draw_date = page.locator("div.toto-draw-date").first.inner_text().strip()
+            url = "https://www.singaporepools.com.sg/en/product/pages/toto_results.aspx"
+            page.goto(url, wait_until="domcontentloaded", timeout=20000)
+            next_jackpot = page.locator("xpath=//div[normalize-space()='Next Jackpot']/following-sibling::span[1]").inner_text().strip()
+            jackpot = "$" + "".join(c for c in next_jackpot if c.isdigit() or c == ",")
+            draw_date = page.locator("div.toto-draw-date").first.inner_text().strip()
 
-    parts = draw_date.split(",")
-    date_part , time_part = parts[1].strip(), parts[2].strip()
+        parts = draw_date.split(",")
+        date_part , time_part = parts[1].strip(), parts[2].strip()
 
-    if date_part ==  date.today().strftime('%d %b %Y'):
-        msg = f"🎰 TOTO Update\nNext Jackpot: {jackpot}\nNext Draw: Tonight, {time_part}"
-    else:
-        msg = f"🎰 TOTO Update\nNext Jackpot: {jackpot}\nNext Draw: {draw_date}"
+        if date_part ==  date.today().strftime('%d %b %Y'):
+            msg = f"🎰 TOTO Update\nNext Jackpot: {jackpot}\nNext Draw: Tonight, {time_part}"
+        else:
+            msg = f"🎰 TOTO Update\nNext Jackpot: {jackpot}\nNext Draw: {draw_date}"
+        
+        send_telegram(msg)
+
+        return {"statusCode": 200}
     
-    send_telegram(msg)
+    if mode == "results":
+        return {"statusCode": 200, "body": "results mode not implemented yet"}
 
-    return {"statusCode": 200}
 
 if __name__ == "__main__":
     lambda_handler({}, None)
